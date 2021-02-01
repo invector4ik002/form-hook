0fac3d0cb34caaac50f174c4d1f5ae5d50a2bb86
invector4ik002 <Vedoone@mail.ru>
2021-02-02 00:07:48 +0700

    add components

--- src/components/Step1/Step1.jsx
+++ src/components/Step1/Step1.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
+import { MainContainer } from '../WrapperComponents/WrapperMainContainer/WrapperMainContainer';
+import { Form } from '../WrapperComponents/WrapperForm/WrapperForm'
 
 export const Step1 = () => {
   return (
-    <h2>Step1</h2>
+    <MainContainer>
+      <Typography component='h2' variant='h5'>Step 1</Typography>
+      <Form>
+
+      </Form>
+    </MainContainer>
   );
 };
---
+++ src/components/WrapperComponents/WrapperForm/WrapperForm.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => {
+  return {
+    root: {
+      width:'100%',
+      marginTop: theme.spacing(1)
+    }
+  }
+})
+/**
+ * Компонент обертка 
+ * noValidate // отключение стандартного повидения HTML валидатора  
+ */
+export const Form = ({ children, ...props }) => {
+  const styles = useStyles()
+
+  return (
+    <form className={styles.root} noValidate {...props}>
+      { children }
+    </form>
+  )
+}
+
